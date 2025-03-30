de4969d036e6795a671dea3f66acffcc7c80d2eb
Shamsutdinov Amir <shamsutdinov.az@phystech.edu>
2025-03-30 12:35:55 +0300

    Changed interpolation function interface
    -Changed bilinear intepolation function for vectorization
    -Added cli for image interpolation

--- src/interpolation/bilinear_interp.py
+++ src/interpolation/bilinear_interp.py
@@ -2,46 +2,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def bilinear_interpolation(x_points: np.array, y_points: np.array, values: np.array):
-    assert x_points.shape == (2,), "x_points должен быть массивом размера (2,)"
-    assert y_points.shape == (2,), "y_points должен быть массивом размера (2,)"
-    assert values.shape == (2, 2), "values должен быть массивом размера (2, 2)"
+def bilinear_interpolation(
+    x_points: np.array, y_points: np.array, values: np.array, x: np.array, y: np.array
+):
+    assert x_points.shape == (2,), "x_points must be an array of shape (2,)"
+    assert y_points.shape == (2,), "y_points must be an array of shape (2,)"
 
     denumerator = (x_points[1] - x_points[0]) * (y_points[1] - y_points[0])
+    assert denumerator != 0, "x_points and y_points should contain different points"
 
-    assert denumerator, "в x_points и y_points должны содержаться разные точки"
+    x_x1 = x - x_points[0]
+    x_x2 = x - x_points[1]
+    y_y1 = y - y_points[0]
+    y_y2 = y - y_points[1]
 
-    def interpolant(x: np.float64, y: np.float64):
-        x_x1 = x - x_points[0]
-        x_x2 = x - x_points[1]
-        y_y1 = y - y_points[0]
-        y_y2 = y - y_points[1]
+    w_a = (-x_x2) * (-y_y2)
+    w_b = x_x1 * (-y_y2)
+    w_c = (-x_x2) * y_y1
+    w_d = x_x1 * y_y1
 
-        return (
-            values[0][0] * (-x_x2) * (-y_y2)
-            + values[1][0] * x_x1 * (-y_y2)
-            + values[0][1] * (-x_x2) * y_y1
-            + values[1][1] * x_x1 * y_y1
-        ) / denumerator
-
-    return interpolant
+    return (
+        values[..., 0, 0] * w_a
+        + values[..., 1, 0] * w_b
+        + values[..., 0, 1] * w_c
+        + values[..., 1, 1] * w_d
+    ) / denumerator
 
 
-def visualize(x_points: np.array, y_points: np.array, num_points: int, interpolant):
-    assert x_points.shape == (2,), "x_points должен быть массивом размера (2,)"
-    assert y_points.shape == (2,), "y_points должен быть массивом размера (2,)"
+def visualize(
+    x_points: np.array, y_points: np.array, num_points: int, values: np.array
+):
+    assert x_points.shape == (2,), "x_points must be an array of shape (2,)"
+    assert y_points.shape == (2,), "y_points must be an array of shape (2,)"
+    assert values.shape == (2, 2), "values must be convertible to 2x2 array"
 
-    x_points = np.linspace(x_points[0], x_points[1], num_points)
-    y_points = np.linspace(y_points[0], y_points[1], num_points)
+    x_grid = np.linspace(x_points[0], x_points[1], num_points)
+    y_grid = np.linspace(y_points[0], y_points[1], num_points)
+    grid_x, grid_y = np.meshgrid(x_grid, y_grid)
 
-    grid_x, grid_y = np.meshgrid(x_points, y_points)
-
-    grid_z = np.vectorize(lambda x, y: interpolant(x, y))(grid_x, grid_y)
+    grid_z = bilinear_interpolation(
+        x_points=x_points, y_points=y_points, values=values, x=grid_x, y=grid_y
+    )
 
     plt.figure(figsize=(8, 6))
-    cp = plt.contourf(grid_x, grid_y, grid_z, cmap="viridis")
-    plt.colorbar(cp)
+    cp = plt.contourf(grid_x, grid_y, grid_z, levels=20, cmap="viridis")
+    plt.colorbar(cp, label="Interpolated Value")
     plt.title("Bilinear Interpolation")
     plt.xlabel("X")
     plt.ylabel("Y")
+    plt.grid(True)
     plt.show()
--- src/interpolation/console.py
+++ src/interpolation/console.py
@@ -1,24 +1,12 @@
 # src/interpolation/console.py
+from pathlib import Path
+
 import click
 import numpy as np
 
 from interpolation import __version__
-from interpolation.bilinear_interp import bilinear_interpolation, visualize
-
-
-@click.command()
-def blt():
-    """
-    This script tests bilinear interpolation
-    """
-
-    x_points = np.array((1, 2))
-    y_points = np.array((1, 2))
-    values = np.array([[1, 2], [3, 4]])
-
-    res = bilinear_interpolation(x_points, y_points, values)
-
-    click.echo(res(0, 0))
+from interpolation.bilinear_interp import visualize
+from interpolation.image_process import process_interpolation
 
 
 @click.command()
@@ -31,8 +19,40 @@ def graph():
     y_points = np.array((1, 2))
     values = np.array([[5, 2], [-1, 4]])
 
-    res = bilinear_interpolation(x_points, y_points, values)
-    visualize(x_points, y_points, 1000, res)
+    visualize(x_points, y_points, 1000, values)
+
+
+@click.command()
+@click.argument("SOURCE")
+@click.argument("OUTPUT")
+@click.argument("WIDTH")
+@click.argument("HEIGHT")
+@click.option(
+    "--force", "-f", is_flag=True, help="Overwriting output file without asking"
+)
+def image_interpolate(source, output, width, height, force):
+    """
+    This script interpolates image from SOURCE to OUTPUT
+
+    OUTPUT image will have size WIDTH*HEIGHT
+    """
+    source = Path(source)
+    output = Path(output)
+
+    if not source.exists():
+        raise ValueError(
+            click.style(f"Input image not found: {source}", fg="red", bold=True)
+        )
+
+    if output.exists() and not force:
+        msg = click.style(f'File "{output}" exists! Overwrite? [y/n]: ', fg="yellow")
+        if input(msg).lower() != "y":
+            click.secho("Cancelled", fg="red")
+            return -1
+    else:
+        output.touch()
+
+    return process_interpolation(source, output, int(width), int(height))
 
 
 @click.group()
@@ -41,8 +61,8 @@ def cli():
     pass
 
 
-cli.add_command(blt)
 cli.add_command(graph)
+cli.add_command(image_interpolate)
 
 if __name__ == "__main__":
     cli()
