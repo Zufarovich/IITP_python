8427ffe46d793940540daa2a9e3d201a18f8dafe
Shamsutdinov Amir <shamsutdinov.az@phystech.edu>
2025-05-19 23:08:56 +0300

    New method(empty:()

---
+++ src/interpolation/bicubic_interpolation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def bicubic_interpolation(
+    x_points: np.ndarray,
+    y_points: np.ndarray,
+    values: np.ndarray,
+    x: np.ndarray,
+    y: np.ndarray,
+) -> np.ndarray:
+    pass
